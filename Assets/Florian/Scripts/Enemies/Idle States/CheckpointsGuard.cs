bf8a236d3f70048342dcc2fe288706c24859a0c8
Florian Boireau <florian23russie@hotmail.fr>
2021-11-29 16:30:44 +0100

    Added a break after the AI reached a checkpoint

--- Assets/Florian/Scripts/Enemies/Idle States/CheckpointsGuard.cs
+++ Assets/Florian/Scripts/Enemies/Idle States/CheckpointsGuard.cs
@@ -6,14 +6,19 @@ using UnityEngine.AI;
 public class CheckpointsGuard : MonoBehaviour
 {
     public List<GameObject> lCheckpoints;
+    public List<float> lWaitEachCP;
+    public float speed;
+
     private NavMeshAgent agent;
     private int i = 0;
+    private bool isWaiting = false;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(lCheckpoints[i++].transform.position); 
+        agent.speed = speed;
+        agent.SetDestination(lCheckpoints[i].transform.position); 
     }
 
     // Update is called once per frame
@@ -21,10 +26,20 @@ public class CheckpointsGuard : MonoBehaviour
     {
         if (!GetComponent<Enemy>().IsPlayerInArea)
         {
-            if (agent.remainingDistance == 0)
-                agent.SetDestination(lCheckpoints[i++].transform.position);
+            if (agent.remainingDistance == 0 && !isWaiting)
+            {
+                StartCoroutine(Wait(lWaitEachCP[i++]));
+                isWaiting = true;
+            }
             if (i >= lCheckpoints.Count)
                 i = 0;
         }
+    }
+
+    IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+        agent.SetDestination(lCheckpoints[i].transform.position);
+        isWaiting = false;
     }
 }
