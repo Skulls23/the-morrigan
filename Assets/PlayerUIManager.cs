863d0da2c9c5ada2adac73b60f8273623297ef30
eddyboreal <eddy.abada.ea@gmail.com>
2022-02-04 17:26:20 +0100

    mask update

---
+++ Assets/PlayerUIManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUIManager : MonoBehaviour
+{
+
+    public void GenerateLP()
+    {
+
+    }
+}
