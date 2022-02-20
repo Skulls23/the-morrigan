8d6fc4ccc65c0eeec618ab8e4988eeb44156f86a
eddyboreal <eddy.abada.ea@gmail.com>
2022-02-20 19:05:45 +0100

    yc hair, liquor holder, liquor shader, scene management iteration 1

---
+++ Assets/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private string[] scenesName;
+
+    private int sceneIndex = 0;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine(LoadAsyncScene(scenesName[sceneIndex++]));
+        }
+    }
+
+    IEnumerator LoadAsyncScene(string sceneName)
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+}
